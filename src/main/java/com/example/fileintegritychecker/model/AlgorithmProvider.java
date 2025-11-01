6622f61a4c24b0c3f6741074e5ab04c16fc27fbe
bitRaybytes <173811320+bitRaybytes@users.noreply.github.com>
2025-11-01 18:52:48 +0100

    Bugs fixed

--- src/main/java/com/example/fileintegritychecker/model/AlgorithmProvider.java
+++ src/main/java/com/example/fileintegritychecker/model/AlgorithmProvider.java
@@ -8,6 +8,27 @@ import java.util.*;
 
 public class AlgorithmProvider {
 
+    /**
+     * Returns a list of available algorithm names for the given encryption category.
+     * Uses the standard Java Security API to fetch all providers.
+     */
+    public static List<String> getAlgorithmsByCategory(EncryptionCategory category) {
+        if (category == null) {
+            return List.of();
+        }
+
+        Set<String> algorithms = Security.getAlgorithms(category.toString());
+        return new ArrayList<>(algorithms);
+    }
+
+    /**
+     * Returns a single string (comma separated) of all available algorithms for display/logging purposes.
+     */
+    public static String getAlgorithmsAsString(EncryptionCategory category) {
+        return String.join(", ", getAlgorithmsByCategory(category));
+    }
+
+
     public static List<String> setEncryptionAlgorithms(EncryptionCategory category) {
         List<String> algorithmList = new ArrayList<>();
         Set<String> algorithms = Security.getAlgorithms(String.valueOf(category));
@@ -21,11 +42,12 @@ public class AlgorithmProvider {
 
     public static List<String> setEncryptionAlgoListStr(EncryptionCategory category){
         List<String> algorithmList = new ArrayList<>();
-        Set<String> algorithms = Security.getAlgorithms(String.valueOf(category).toString()
-                .replace("[", "")
-                .replace("]", ""));
+
+
+        Set<String> algorithms = Security.getAlgorithms(String.valueOf(category));
 
         for (String algorithm : algorithms) {
+            algorithm.split(", ");
             algorithmList.add(algorithm);
         }
 
@@ -48,28 +70,35 @@ public class AlgorithmProvider {
         return mdList;
     }
 
-    public static String setEncryptionAlgorithmsAsString(EncryptionCategory category) {
-//        return Arrays.asList(setAlgorithmsCategory(EncryptionCategory.MESSAGE_DIGEST));
-        String categoryAlgorithms = Security.getAlgorithms(String.valueOf(category)).toString()
+    public static String setEncryptionAlgoString(EncryptionCategory category){
+        String algoCategory = Security.getAlgorithms(String.valueOf(category)).toString()
                 .replace("[","")
                 .replace("]","");
+        return algoCategory;
+    }
 
+    public static String[] setEncryptionAlgorithmsAsString(EncryptionCategory category) {
+//        return Arrays.asList(setAlgorithmsCategory(EncryptionCategory.MESSAGE_DIGEST));
+
+
+        String[] categoryAlgorithms = (Security.getAlgorithms(String.valueOf(category)).toString()
+                .replace("[","")
+                .replace("]","").split(", "));
+
+        for (String str : categoryAlgorithms){
+            categoryAlgorithms.toString();
+        }
 
         return categoryAlgorithms;
     }
 
     static void main(String[] args) {
-//        System.out.println(setEncryptionAlgorithms(EncryptionCategory.MESSAGEDIGEST));
-//        System.out.println(setEncryptionAlgorithms(EncryptionCategory.CIPHER));
-//        System.out.println(setEncryptionAlgorithms(EncryptionCategory.KEYGENERATOR));
-//        System.out.println(setEncryptionAlgorithms(EncryptionCategory.KEYGENERATOR, 256));
-//        System.out.println(setEncryptionAlgorithms(EncryptionCategory.MAC));
-//        System.out.println(setEncryptionAlgorithms(EncryptionCategory.SECURERANDOM));
-//        System.out.println(setEncryptionAlgorithms(EncryptionCategory.KEYFACTORY));
-//        System.out.println(setEncryptionAlgorithms(EncryptionCategory.SIGNATURE));
-        System.out.println(setEncryptionAlgoListStr(EncryptionCategory.CIPHER));
-        System.out.println(setEncryptionAlgoListStr(EncryptionCategory.MAC));
+//        System.out.println(setEncryptionAlgoListStr(EncryptionCategory.MAC));
         System.out.println(setEncryptionAlgoListStr(EncryptionCategory.MESSAGEDIGEST));
+
+//        List<String> list = List.of(setEncryptionAlgorithmsAsString(EncryptionCategory.MESSAGEDIGEST));
+//        String str = Security.getProperty(EncryptionCategory.MESSAGEDIGEST.toString());
+//        System.out.println(list);
 
 
     }
