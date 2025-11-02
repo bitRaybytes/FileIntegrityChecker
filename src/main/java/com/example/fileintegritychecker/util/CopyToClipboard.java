daf775086a83a8123cba60442b2993dc3dd225da
bitRaybytes <173811320+bitRaybytes@users.noreply.github.com>
2025-11-02 23:13:42 +0100

    New class added: handling copying content

--- src/main/java/com/example/fileintegritychecker/util/CopyToClipboard.java
+++ src/main/java/com/example/fileintegritychecker/util/CopyToClipboard.java
@@ -1,4 +1,20 @@
 package com.example.fileintegritychecker.util;
 
+import javafx.scene.control.Label;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+
 public class CopyToClipboard {
+
+    public CopyToClipboard(){}
+
+    /** --- Copy results --- */
+    public void copyToClipboard(Label label) {
+        // Copy the result to clipboard
+        ClipboardContent content = new ClipboardContent();
+        // Put the text from the label into the clipboard content
+        content.putString(label.getText());
+        // Copy the content to the system clipboard
+        Clipboard.getSystemClipboard().setContent(content);
+    }
 }
