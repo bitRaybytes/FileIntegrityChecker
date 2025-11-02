daac74011b6236cd284bf4da13829af4b9a5edda
bitRaybytes <173811320+bitRaybytes@users.noreply.github.com>
2025-11-02 23:14:25 +0100

    minor changes due to adding new classes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,9 @@ module com.example.fileintegritychecker {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.ikonli.core;
     requires javafx.graphics;
+    requires jdk.jdi;
+    requires javafx.base;
+    requires com.example.fileintegritychecker;
 
     opens com.example.fileintegritychecker to javafx.fxml;
     opens com.example.fileintegritychecker.controller to javafx.fxml;
